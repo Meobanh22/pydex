43247e39cabaa9779feb3167938ab81e4676d27e
meobanh <D.Manh2208@gmail.com>
2026-09-02 21:35:29 +0700

    feat(schema): update schema to support function parameters

--- schema.sql
+++ schema.sql
@@ -2,13 +2,15 @@ CREATE TABLE functions (
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     official_docstring TEXT,
-    my_notes TEXT
+    my_notes TEXT,
+    discovered BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE arguments (
     id SERIAL PRIMARY KEY,
     function_id INT NOT NULL REFERENCES functions(id) ON DELETE CASCADE,
     name TEXT NOT NULL,
-    type TEXT,
-    order_index INT NOT NULL
+    order_index INT NOT NULL,
+    default_value TEXT,
+    required BOOLEAN DEFAULT TRUE
 );

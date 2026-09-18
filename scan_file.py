3491d19ef32355344c1a0131a7c602ac4c98d964
meobanh <D.Manh2208@gmail.com>
2026-09-18 21:31:28 +0700

    feat(scanner): upgrade AST scanner to detect library imports and attribute calls

--- scan_file.py
+++ scan_file.py
@@ -4,16 +4,7 @@ import inspect
 import sys
 from db import get_connection
 
-def find_builtin_call(file_path):
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            tree = ast.parse(f.read())
-    except FileNotFoundError:
-        print(f"Error: {file_path} does not exist")
-        return None
-    except SyntaxError:
-        print(f"Error: {file_path} has syntax error")
-        return None
+def find_builtin_call(tree):
     builtins_name = {
         name for name, obj in builtins.__dict__.items()
         if (inspect.isbuiltin(obj) or inspect.isclass(obj))
@@ -25,11 +16,43 @@ def find_builtin_call(file_path):
             func_name = node.func.id
             if func_name in builtins_name:
                 found.add(func_name)
-    return found
+    return [("builtins", f"{name}") for name in found]
+
+def find_library_call(tree, module_map, func_map, module):
+    found = set()
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Call) and isinstance(node.func, ast.Attribute):
+            func_name = node.func.attr
+            if isinstance(node.func.value, ast.Name) and node.func.value.id in module_map and module_map[node.func.value.id] in module:
+                found.add((module_map[node.func.value.id], func_name))
+        elif isinstance(node, ast.Call) and isinstance(node.func, ast.Name):
+            func_name = node.func.id
+            if func_name in func_map and func_map[func_name][0] in module:
+                found.add((func_map[func_name][0], func_map[func_name][1]))
+    return list(found)
+
+def build_import_maps(tree):
+    module_map = {}
+    func_map = {}
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            for alias in node.names:
+                if alias.asname == None:
+                    module_map[alias.name] = alias.name
+                else:
+                    module_map[alias.asname] = alias.name
+        elif isinstance(node, ast.ImportFrom):
+            for alias in node.names:
+                if alias.asname==None:
+                    func_map[alias.name] = (node.module, alias.name)
+                else:
+                    func_map[alias.asname] = (node.module, alias.name)
+    return module_map, func_map
+
 def lookup_and_mark(names, conn):
     with conn.cursor() as cur:
         for name in names:
-            cur.execute("SELECT id,discovered FROM functions WHERE name = %s", (name,))
+            cur.execute("SELECT f.id, f.discovered FROM functions f LEFT JOIN modules m ON f.module_id = m.id WHERE f.name = %s AND m.name = %s", (name[1], name[0]))
             row = cur.fetchone()
             if row:
                 func_id, discovered = row
@@ -39,12 +62,30 @@ def lookup_and_mark(names, conn):
             else:
                 print(f"Function '{name}' not found.")
     conn.commit()
+
+def scan_file(file_path):
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            tree = ast.parse(f.read())
+    except FileNotFoundError:
+        print(f"Error: {file_path} does not exist")
+        return None
+    except SyntaxError:
+        print(f"Error: {file_path} has syntax error")
+        return None
+    module = {'math', 'random', 'json'}
+    builtins = find_builtin_call(tree)
+    module_map, func_map = build_import_maps(tree)
+    stdlibs =  find_library_call(tree, module_map, func_map, module)
+    result = builtins + stdlibs
+    return result
+
 if __name__ == "__main__":
     if len(sys.argv) <2:
         print("Usage: python scan_file.py <file_path>") 
         sys.exit(1)
     file_path = sys.argv[1]
-    result = find_builtin_call(file_path)
+    result = scan_file(file_path)
     if result is None:
         sys.exit(1)
     if not result:
